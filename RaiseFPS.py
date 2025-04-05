e44eabbab0fa6779dc83f54c771846480f9013f8
mOntey1 <alan.sawicki@opoczta.pl>
2025-04-05 20:13:06 +0200

    added restore point

--- RaiseFPS.py
+++ RaiseFPS.py
@@ -7,6 +7,8 @@ from io import BytesIO
 import LowMode
 import MediumMode
 import ProMode
+import backup
+
 def optimize(level):
     if level == "low":
         LowMode.optimize_low()
@@ -17,6 +19,9 @@ def optimize(level):
     elif level == "pro":
         ProMode.optimize_pro()
         messagebox.showinfo("Optymalizacja", "Optymalizacja PRO zakończona pomyślnie!")
+    elif level == "backup":
+        backup.backup("Punkt przywracania wykonany przez RaiseFPS")
+        messagebox.showinfo("Backup", "Backup wykonano pomyślnie!")
 
 def create_gui():
     root = tk.Tk()
@@ -29,6 +34,7 @@ def create_gui():
     low_color = "#FFDC57"       # zieleń
     medium_color = "#7EDA53"    # żółty
     pro_color = "#FF5558"      # czerwony
+    backup_color = "#2D2D2D"    # ciemnoszary
 
     image_url = 'https://i.fmfile.com/I3L9suyZSKkKsLtt1FUmd/RaiseFPS.png'
     response = requests.get(image_url)
@@ -56,32 +62,38 @@ def create_gui():
         bg=bg_color
     )
     subtitle_label.pack(pady=(0, 20))
+    
     button_frame = tk.Frame(root, bg=bg_color)
-    button_frame.pack(pady=20)
+    button_frame.pack(pady=5)
 
-    def create_button(text, color, command):
+    def create_button(text, color, command, font_size=16, btn_padx=60, btn_pady=15):
         button = tk.Button(
-            button_frame, text=text,
+            button_frame, 
+            text=text,
             command=command,
-            bg=color, fg="black",
+            bg=color, 
+            fg="black",
             activebackground=color,
             activeforeground="white",
-            font=("Verdana", 16, "bold"),
-            padx=60, pady=15,
-            bd=0, relief="flat",
+            font=("Verdana", font_size, "bold"),
+            padx=btn_padx, 
+            pady=btn_pady,
+            bd=0, 
+            relief="flat",
             highlightthickness=0,
             cursor="hand2"
         )
-
         return button
 
     low_button = create_button("Low", low_color, lambda: optimize("low"))
     medium_button = create_button("Medium", medium_color, lambda: optimize("medium"))
     pro_button = create_button("Pro", pro_color, lambda: optimize("pro"))
+    backup_button = create_button("Zrób punkt przywracania", backup_color, lambda: optimize("backup"), font_size=10, btn_padx=30, btn_pady=12)
 
     low_button.pack(pady=10, fill=tk.X, padx=50)
     medium_button.pack(pady=10, fill=tk.X, padx=50)
     pro_button.pack(pady=10, fill=tk.X, padx=50)
+    backup_button.pack(pady=10, fill=tk.X, padx=10)
 
     footer_label = tk.Label(
         root,
---
+++ backup.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import sys
+
+def backup(description):
+    if not is_admin():
+        print("Musisz uruchomić jako administrator.")
+        return
+    command = f"powershell.exe -Command \"Checkpoint-Computer -Description '{description}' -RestorePointType 'MODIFY_CONFIG'\""
+    try:
+        subprocess.run(command, check=True, shell=True)
+        print(f"Punkt przywracania stworzony!")
+    except subprocess.CalledProcessError as e:
+        print(f"Błąd podczas tworzenia punktu przywracania: {e}")
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
